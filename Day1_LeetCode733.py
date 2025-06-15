09b2361a747ed4b80c74b3581aac86f2f8ca93e0
nam59 <148378887+nam-bhutani@users.noreply.github.com>
2025-06-15 13:20:45 +0530

    Create Day1.py
    Solved LeetCode Problem 733

---
+++ Day1_LeetCode733.py
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][]=new boolean[image.length][image[0].length];
+        helper(image,sr,sc,color,vis,image[sr][sc]);
+        return image;
+    }
+    public void helper(int[][] image, int sr, int sc, int color,boolean[][] vis,int orgColor){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=orgColor || vis[sr][sc]){
+            return;
+        }
+        image[sr][sc] = color;
+        vis[sr][sc] = true;
+        //left
+        helper(image,sr,sc-1,color,vis,orgColor);
+        //right
+        helper(image,sr,sc+1,color,vis,orgColor);
+        //up
+        helper(image,sr-1,sc,color,vis,orgColor);
+        //down
+        helper(image,sr+1,sc,color,vis,orgColor);
+    }
+}
