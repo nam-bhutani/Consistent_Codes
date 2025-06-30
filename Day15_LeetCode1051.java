afb5e8b0d99b9d15ef9f579a43f320423839bbea
nam59 <148378887+nam-bhutani@users.noreply.github.com>
2025-06-30 21:13:06 +0530

    Create Day15_LeetCode1051.java

---
+++ Day15_LeetCode1051.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n=heights.length;
+        int temp[]=new int[n];
+        for(int i=0;i<n;i++){
+            temp[i]=heights[i];
+        }
+        Arrays.sort(temp);
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(heights[i] != temp[i]){
+                count+=1;
+            }
+        }
+        return count;
+    }
+}
